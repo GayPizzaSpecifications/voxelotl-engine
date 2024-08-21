cad6418cff3ea2d64d980ac8b360cc0507eefd68
a dinosaur <dino@a-dinosaur.com>
2024-08-22 06:24:30 +1000

    organise maths helpers

---
+++ Sources/Voxelotl/Math/FloatExtensions.swift
@@ -0,0 +1,10 @@
+public extension FloatingPoint {
+  @inline(__always) var degrees: Self { self * (180 / Self.pi) }
+  @inline(__always) var radians: Self { self * (Self.pi / 180) }
+
+  @inline(__always) func lerp(_ a: Self, _ b: Self) -> Self { b * self + a * (1 - self) }
+  @inline(__always) func mlerp(_ a: Self, _ b: Self) -> Self { a + (b - a) * self }
+
+  @inline(__always) func clamp(_ a: Self, _ b: Self) -> Self { min(max(self, a), b) }
+  @inline(__always) var saturated: Self { self.clamp(0, 1) }
+}
--- Sources/Voxelotl/Math/VectorExtensions.swift
+++ Sources/Voxelotl/Math/VectorExtensions.swift
@@ -1,14 +1,3 @@
-public extension FloatingPoint {
-  @inline(__always) var degrees: Self { self * (180 / Self.pi) }
-  @inline(__always) var radians: Self { self * (Self.pi / 180) }
-
-  @inline(__always) func lerp(_ a: Self, _ b: Self) -> Self { b * self + a * (1 - self) }
-  @inline(__always) func mlerp(_ a: Self, _ b: Self) -> Self { a + (b - a) * self }
-
-  @inline(__always) func clamp(_ a: Self, _ b: Self) -> Self { min(max(self, a), b) }
-  @inline(__always) var saturated: Self { self.clamp(0, 1) }
-}
-
 extension SIMD3 where Scalar: FloatingPoint {
   @inline(__always) static var X: Self      { Self(1, 0, 0) }
   @inline(__always) static var Y: Self      { Self(0, 1, 0) }
