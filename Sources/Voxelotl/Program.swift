b0ccd3b335d89716a4111ea2b9732b4b0559df13
Alex Zenla <alex@edera.dev>
2024-09-03 05:00:28 -0400

    use main.m and SDL_main functionality

---
+++ Sources/Voxelotl/Program.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+@objc public class Program: NSObject {
+  @objc public static func run() -> Int32 {
+    Thread.current.qualityOfService = .userInteractive
+
+    let app = Application(
+      delegate: Game(),
+      configuration: ApplicationConfiguration(
+      frame: Size(1280, 720),
+      title: "Voxelotl Demo",
+      flags: [ .resizable, .highDPI ],
+      vsyncMode: .on(interval: 1)))
+
+    return app.run()
+  }
+}
